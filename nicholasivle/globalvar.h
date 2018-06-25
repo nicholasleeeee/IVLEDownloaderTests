23f5ad4218d24b58610e9c8fbb489ed9d25983a9
Geraldcdx <35788598+Geraldcdx@users.noreply.github.com>
2018-06-25 11:14:19 +0800

    Update globalvar.h

--- nicholasivle/globalvar.h
+++ nicholasivle/globalvar.h
@@ -1,3 +1,4 @@
+//A designed .h file to create all possible global variables required in the code.
 #ifndef GLOBALVAR_H
 #define GLOBALVAR_H
 
@@ -6,7 +7,7 @@
 #include <QVariant>
 
 
-    extern QString APIKEY;
+    extern QString APIKEY;//An API key uses the "extern" keyword to be stored as a global variable
 
 
 #endif // GLOBALVAR_H
