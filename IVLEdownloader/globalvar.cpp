ed606b3dc9fa11f8eff13fb9f41fd1c81b8e1f07
Unknown <nicholaslwh97@gmail.com>
2018-07-31 00:28:32 +0800

    321

--- IVLEdownloader/globalvar.cpp
+++
@@ -1,7 +0,0 @@
-// The file to test if the global variables could be changed from any cpp file
-#include "globalvar.h"
-
-QString APIKEY= "NULL";
-QString DIRECTORY="NULL";
-QString TOKEN="NULL";
-
--- IVLEdownloader/parsing.cpp
+++
@@ -1,6 +0,0 @@
-#include "parsing.h"
-
-Parsing::Parsing()
-{
-
-}
